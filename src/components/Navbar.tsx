24572c8b5f7a23576218c2e16bf4b89cc58483b6
Christopher Mangan <chrisjm66@gmail.com>
2024-10-25 00:13:43 -0400

    job simulator

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,17 +2,17 @@ import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <div className='flex flex-nowrap justify-center items-center top-0 p-6 mt-0 mb-5 w-screen bg-cyan-950'>
-            <div className='absolute flex flex-nowrap left-10'>
-                <h1 className='text-3xl'>ChrisMangan</h1>
-                <h1 className='text-3xl font-bold text-violet-400'>.net</h1>
+        <div className='flex flex-nowrap justify-center items-center top-0 p-6 mt-0 w-screen bg-cyan-950'>
+            <div className='absolute flex flex-nowrap flex-row left-10'>
+                <h1 className='text-2xl invisible lg:visible'>ChrisMangan</h1>
+                <h1 className='text-2xl font-bold text-violet-400 invisible lg:visible'>.net</h1>
             </div>
-            <div className="">
-                <nav>
-                    <NavLink to='/' className='text-lg font-bold text-gray-100 no-underline hover:bg-sky-200 rounded-md py-3 px-6 mx-4 transition ease-in-out delay-15 duration-300'>Home</NavLink>
-                    <NavLink to='/about' className='text-lg font-bold text-gray-100 no-underlin hover:bg-sky-200 rounded-md py-3 px-6 mx-4 transition ease-in-out delay-15 duration-300'>About Me</NavLink>
-                    <NavLink to='/contact' className='text-lg font-bold text-gray-100 no-underline hover:bg-sky-200 rounded-md py-3 px-6 mx-4 transition ease-in-out delay-15 duration-300'>Contact</NavLink>
-                    <NavLink to='/portfolio' className='text-lg font-bold text-gray-100 no-underline hover:bg-sky-200 rounded-md py-3 px-6 mx-4 transition ease-in-out delay-15 duration-300'>Portfolio</NavLink>
+            <div>
+                <nav className="flex flex-row flex-nowrap">
+                    <NavLink to='/' className='text-sm md:text-lg font-bold text-gray-100 no-underline hover:bg-sky-500 rounded-md py-3 px-6 mx-2 md:mx-4 transition ease-in-out delay-15 duration-300'>Home</NavLink>
+                    <NavLink to='/about' className='text-sm md:text-lg font-bold text-gray-100 no-underlin hover:bg-sky-500 rounded-md py-3 px-6 mx-2 md:mx-4 transition ease-in-out delay-15 duration-300'>About Me</NavLink>
+                    <NavLink to='/contact' className='text-sm md:text-lg font-bold text-gray-100 no-underline hover:bg-sky-500 rounded-md py-3 px-6 mx-2 md:mx-4 transition ease-in-out delay-15 duration-300'>Contact</NavLink>
+                    <NavLink to='/portfolio' className='text-sm md:text-lg font-bold text-gray-100 no-underline hover:bg-sky-500 rounded-md py-3 px-6 mx-2 md:mx-4 transition ease-in-out delay-15 duration-300'>Portfolio</NavLink>
                 </nav>
            </div>
         </div>

8527341aa581368b3b2d9d834c2b388a66f63c99
Christopher Mangan <chrisjm66@gmail.com>
2024-10-25 13:28:10 -0400

    mobile improvements

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,22 @@
+import { NavLink } from "react-router-dom"
 import ProjectDisplay from "./ProjectDisplay"
 
 const Home = () => {
     return (
         <div>
-            <div className='flex justify-center flex-col bg-cyan-900 py-5'>
-                <h1>Hello! My name is Chris. Welcome to my website.</h1>
+            <div className='flex justify-center items-center flex-col bg-cyan-900 py-5'>
+                <h1 className='text-2xl sm:text-4xl'>Hello! My name is Chris. Welcome to my website.</h1>
 
-                <div className='m-5 text-3xl flex flew-row justify-center'>
+                <div className='m-5 text-lg sm:text-3xl text-nowrap md:text-wrap flex flew-row justify-center'>
                     <h2>I enjoy</h2>
                     <h2 className="text-amber-500 mx-2">web development, aviation, and music.</h2>
                 </div>
+
+                <NavLink to='/about' className='bg-gray-800 p-4 rounded-xl border-2 border-gray-800 w-50 h-15 transition hover:border-amber-500'>Learn more about me</NavLink>
             </div>
 
             <div className='pt-5'>
-                <h1>My Projects</h1>
+                <h1 className='font-bold'>My Projects</h1>
 
                 <div className='flex flex-row justify-center p-10'>
                     <ProjectDisplay name='This Website' description='This website is a project in and of itself! It uses React and Node primarily!' img='obama.png' localHref='personal-website' githubHref='https://github.com/chrisjm66/new-personal-website' />
--- src/components/ProjectDisplay.tsx
+++ src/components/ProjectDisplay.tsx
@@ -4,19 +4,19 @@ import { NavLink } from "react-router-dom"
 
 const ProjectDisplay = ({name, description, img, githubHref, localHref}: any) => {
     return (
-        <div className='w-80 h-96 sm:w-120 sm:h-136  transition duration-300 border-4 border-gray-500 bg-cyan-800 flex flex-col justify-center align-top justify-items-center rounded-xl px-6 py-2 hover:scale-105 hover:bg-cyan-700'>
-            <h1 className='m-2 text-left font-bold'>{name}</h1>
+        <div className='w-80 h-1/2 sm:w-120 sm:h-136  transition duration-300 border-4 border-gray-500 bg-cyan-800 flex flex-col justify-center align-top justify-items-center rounded-xl px-6 py-2 hover:scale-105 hover:bg-cyan-700'>
+            <h1 className='m-2 text-left font-semibold'>{name}</h1>
 
             
             <img className='w-1/2 rounded-md border-' src={img} alt='hi'/>
             
             
             
-            <h2 className='text-left mt-5'>{description}</h2>
+            <h2 className='text-left text-lg italic mt-5'>{description}</h2>
 
-            <NavLink to={githubHref} className='flex flex-row items-center transition duration-300 hover:text-green-300'>
+            <NavLink to={githubHref} className='flex flex-row items-center transition duration-300 hover:text-green-500'>
                 <FontAwesomeIcon className='w-10 h-10' icon={faGithub}/>
-                <h3 className='font-mono text-lg ml-2 my-4'>chrisjm66/new-personal-website</h3>
+                <h3 className='font-mono font-semibold text-lg ml-2 my-4 text-left'>chrisjm66/new-personal-website</h3>
                 
             </NavLink>
 

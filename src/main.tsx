7ea9a7002bc36acac9cbe5b6019252ddbc33b719
Christopher Mangan <chrisjm66@gmail.com>
2024-12-12 23:31:16 -0500

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import React, { StrictMode } from 'react'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

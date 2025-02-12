c5b530b91bf2e26f7f263a7ace32c993c865a4df
Chris Mangan <120677529+chrisjm66@users.noreply.github.com>
2025-02-12 14:47:55 -0500

    Update Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,7 +1,11 @@
 const Portfolio = () => {
     return (
-        <h1>Portfolio</h1>
-        <h2>Bill stop reading my website</h2>
+        <div>
+    
+            <h1>Portfolio</h1>
+            <h2>Bill stop reading my website</h2>
+
+        </div>
     )
 }
 

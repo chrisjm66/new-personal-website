bb768573eca2962a8e110e88ba9bfe88f4e8456a
Chris Mangan <120677529+chrisjm66@users.noreply.github.com>
2025-02-21 16:32:33 -0500

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
-import { Link, NavLink } from "react-router-dom"
-import ProjectDisplay from "./ProjectDisplay"
+import { Link } from "react-router-dom"
+//import ProjectDisplay from "./ProjectDisplay"
 
 
 const Home = () => {
